81af0d89258521abde807100e99527e109126502
Bartosz <26346113+bbasinski@users.noreply.github.com>
2021-10-12 18:38:12 +0200

    Sidebar component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
-  <div id="app" class="flex flex-row">
-    <div id="nav" class="flex flex-col">
-      <router-link to="/">Home List</router-link>
-      <router-link to="/category">Add Category</router-link>
-      <router-link to="/home">Add home</router-link>
-    </div>
+  <div id="app" class="flex flex-row h-full">
+    <sidebar/>
     <div class="p-10 w-full">
       <router-view/>
     </div>
   </div>
 </template>
 
+
+<script>
+import Sidebar from "@/components/Sidebar";
+export default {
+  components: {Sidebar}
+}
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -19,17 +22,8 @@
   color: #2c3e50;
 }
 
-#nav {
-  padding: 30px;
+html, body {
+
 }
 
-#nav a {
-  @apply my-2 whitespace-nowrap;
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="bg-green-200">
+    <div class="flex justify-center mt-6">
+      <svg xmlns="http://www.w3.org/2000/svg" class="h-40 w-40" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
+      </svg>
+    </div>
+    <div id="nav" class="flex flex-col">
+      <router-link to="/">Domki</router-link>
+      <router-link to="/category">Kategorie domków</router-link>
+      <router-link to="/animals">Zwierzęta</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+}
+</script>
+
+<style scoped>
+
+#nav {
+  @apply p-10
+}
+
+#nav a {
+  @apply whitespace-nowrap p-3;
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  @apply bg-white rounded;
+  color: #42b983;
+}
+</style>
